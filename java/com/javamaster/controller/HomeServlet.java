15737f2e4fc9ba2e0bc9989d70240be993b81d55
zhivenkov <zhivenkov@mail.ru>
2019-12-08 18:45:02 +0300

    Change serialVersionUID = 102831973239L

--- java/com/javamaster/controller/HomeServlet.java
+++ java/com/javamaster/controller/HomeServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class HomeServlet
  */
 public class HomeServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 102831973239L;
        
     /**
      * @see HttpServlet#HttpServlet()
